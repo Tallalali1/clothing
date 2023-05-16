d7df6ba058d0a0dbd66a6510f38280992d14731a
Tallal <=>
2023-05-16 14:22:04 +0500

    added categories and rendered there title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: "Hats"
+    },
+    {
+      id: 2,
+      title: "Jackets"
+    },
+    {
+      id: 3,
+      title: "Jeans"
+    },
+    {
+      id: 4,
+      title: "Men"
+    },
+    {
+      id: 5,
+      title: "Women"
+    }
+  ]
+
   return (
-    <div className="App">
-    Hello 
+    <div className="categories-container">
+    {categories.map((category) => (
+      <div className="category-container">
+        <div className="img"/>
+        <div className="category-body-container">
+          <h2>{category.title}</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+    ))}
     </div>
   );
 }
